fad7d471c95f5960c8a28fa836acc14bed81b3ce
AnguloBE <bry555xd@gmail.com>
2024-02-03 16:46:42 -0700

    newTarjeta

--- app/src/main/java/com/example/sistema/Tarjeta.kt
+++ app/src/main/java/com/example/sistema/Tarjeta.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import java.time.LocalDate
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
 import kotlin.random.Random
 
 class Tarjeta(
@@ -16,16 +18,33 @@ class Tarjeta(
     private var nombreUsuario:String
 )
 {
+    constructor() : this(
+        numTarjeta = 0,
+        fechaVencimiento = LocalDate.now(),
+        cvv = 0,
+        dineroDisponible = 0.0,
+        pinSeguridad = 0,
+        nombreUsuario = ""
+    )
 
 
 
-    private fun newTarjeta(nombreUsuario: String, pinSeguridad: Int){
-        var numTarjeta: String = ""
-        val random = Random
-        for (i in 0 until 16){
+     fun newTarjeta(nombreUsuario: String, pinSeguridad: Int){
+         var numTarjeta: String = ""
+         var fechaVencimiento: LocalDate = LocalDate.now().plusMonths(15).with(TemporalAdjusters.lastDayOfMonth())
+         var cvv: String= ""
+         val random = Random
+         for (i in 0 until 16){
             numTarjeta += random.nextInt(10)
-        }
-        Log.d("numTarjeta: ", numTarjeta)
+         }
+
+         for (i in 0 until 3){
+             cvv += random.nextInt(10)
+         }
+         val tarjeta: Tarjeta= Tarjeta(numTarjeta.toInt(),fechaVencimiento,cvv.toInt(),0.0,pinSeguridad,nombreUsuario)
+
+
+        Log.d("numTarjeta: ", "numTarjeta: $numTarjeta fecha vencimiento: $fechaVencimiento cvv: $cvv")
 
 
 
